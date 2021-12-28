a4cc23a66d0531e19116364b56e4c2e6c1796b30
Drake Dwornik <drake@drakedwornik.com>
2021-12-28 16:14:06 -0500

    form renders well enough, starting handling

--- Tracker/forms.py
+++ Tracker/forms.py
@@ -1,10 +1,16 @@
 from django import forms
+import datetime
 
 
 class MailingForm(forms.Form):
-    customer = forms.CharField(label='customer name', max_length=100)
-    mailing_name = forms.CharField(label='mailing name', max_length=50)
-    mailing_dropoff_date = forms.DateField(label='mailing dropoff date')
-    mailing_type_description = forms.CharField(label='mailing type', max_length=50)
-    job_number = forms.CharField(label='job_number', max_length=15)
-    file = forms.FileField(label='Mailing File')
+    customer = forms.CharField(label='customer name', max_length=100,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mailing_name = forms.CharField(label='mailing name', max_length=50,
+                                   widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mailing_dropoff_date = forms.DateField(label='mailing dropoff date',
+                                           widget=forms.widgets.DateInput(attrs={'type': 'date', 'class': 'form-control'}), initial=datetime.date.today)
+    mailing_type_description = forms.CharField(label='mailing type', max_length=50,
+                                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    job_number = forms.CharField(label='job_number', max_length=15,
+                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
+    file = forms.FileField(label='Mailing File', widget=forms.TextInput(attrs={'class': 'form-control'}))
