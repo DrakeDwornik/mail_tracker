c87adf05c6b96425f050b36a7518c69c74e2f99f
Drake Dwornik <drake@drakedwornik.com>
2021-12-30 18:28:21 -0500

    working with dropzone.js

--- Tracker/file_handler.py
+++ Tracker/file_handler.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import logging
 
+
 #
-def handle_uploaded_file(file, mailing_id):
-    pass
-#     mailing_list = pd.read_csv(file)
-#     mailing_list = mailing_list[]
-#     logging.error("\n" + str(mailing_list.head(3)))
-#     x = mailing_list['PST_IMB']
-#     logging.error(x)
+def handle_uploaded_file(file, mailing_pk):
+    mailing_list = pd.read_csv(file)
+    logging.error(str(mailing_list.head(3)))
+    # mailing_list = mailing_list['recipient', 'title', 'company',
+    #                             'address1', 'address2', 'address_extra', 'city', 'state', 'zip',
+    #                             'record_number', 'oel', 'imb_numeric', 'imb_alpha', 'sort_order',
+    #                             'bundle_number', 'container_number', 'manifest_key']
+    # logging.error(x)
--- Tracker/forms.py
+++ Tracker/forms.py
@@ -2,17 +2,17 @@ from django import forms
 import datetime
 
 
-
 class MailingForm(forms.Form):
     customer = forms.CharField(label='customer name', max_length=100,
                                widget=forms.TextInput(attrs={'class': 'form-control'}))
     mailing_name = forms.CharField(label='mailing name', max_length=50,
                                    widget=forms.TextInput(attrs={'class': 'form-control'}))
     mailing_dropoff_date = forms.DateField(label='mailing dropoff date',
-                                           widget=forms.widgets.DateInput(attrs={'type': 'date', 'class': 'form-control'}), initial=datetime.date.today)
+                                           widget=forms.widgets.DateInput(
+                                               attrs={'type': 'date', 'class': 'form-control'}),
+                                           initial=datetime.date.today)
     mailing_type_description = forms.CharField(label='mailing type', max_length=50,
                                                widget=forms.TextInput(attrs={'class': 'form-control'}))
     job_number = forms.CharField(label='job_number', max_length=15,
                                  widget=forms.TextInput(attrs={'class': 'form-control'}))
-    file = forms.FileField(label='Mailing File')
-
+    # file = forms.FileField(label='Mailing File')
