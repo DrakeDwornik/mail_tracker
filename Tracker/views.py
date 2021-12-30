e545ffa5324d23658add519084b74b229acc2ad1
Drake Dwornik <drake@drakedwornik.com>
2021-12-30 07:16:53 -0500

    better use of bootstrap, forms starting to work

--- Tracker/views.py
+++ Tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import MailingForm
+from .models import Mailing
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -9,8 +10,13 @@ def add_mailing(request):
     if request.method == 'POST':
         form = MailingForm(request.POST)
         if form.is_valid():
-            pass
-
+            mailing = Mailing()
+            mailing.customer = request.POST.get('customer')
+            mailing.mailing_dropoff_date = request.POST.get('mailing_dropoff_date')
+            mailing.mailing_type_description = request.POST.get('mailing_type_description')
+            mailing.job_number = request.POST.get('job_number')
+            mailing.save()
+        return render(request, 'add_mailing.html', {'form': form})
     else:
         form = MailingForm()
 
