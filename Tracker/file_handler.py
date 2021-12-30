ce7f9626e4445578b4dfce3d45bf971f8c48ec76
Drake Dwornik <drake@drakedwornik.com>
2021-12-30 16:23:50 -0500

    filled out models, and got some preliminary file handling

--- Tracker/file_handler.py
+++ Tracker/file_handler.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import logging
 
-
+#
 def handle_uploaded_file(file, mailing_id):
-    mailing_list = pd.read_csv(file)
-    logging.error(mailing_list.head())
+    pass
+#     mailing_list = pd.read_csv(file)
+#     mailing_list = mailing_list[]
+#     logging.error("\n" + str(mailing_list.head(3)))
+#     x = mailing_list['PST_IMB']
+#     logging.error(x)
