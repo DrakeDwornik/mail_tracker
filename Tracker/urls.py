17514cffb3e1bc49ded54007c53c15ef2e024943
Drake Dwornik <drake@drakedwornik.com>
2022-01-03 11:05:15 -0500

    removed data from project folder, some changes to start getting data to display

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', name='index', view=views.index),
     path('add_mailing', name='add_mailing', view=views.add_mailing),
     path('add_scans', name='add_scans', view=views.add_scans),
-    path('add_mailing_list', name='add_mailing_list', view=views.add_mailing_list)
+    path('add_mailing_list', name='add_mailing_list', view=views.add_mailing_list),
+    path('mailing_stats', name='mailing_stats', view=views.mailing_stats)
 ]
--- Tracker/views.py
+++ Tracker/views.py
@@ -48,8 +48,44 @@ def add_mailing_list(request):
     if request.method == 'POST':
         file = request.FILES['file']
         mailing_id = request.POST.get('mailing_id')
-        logging.error("here")
+        # logging.error("here")
         # return render(request, 'add_mailing.html')
         # read_headers(file)
         handle_uploaded_list(file, mailing_id)
         return render(request, 'add_scans.html')
+
+
+def mailing_stats(request):
+    # mailing_id = 53
+
+    mailing_id = int(request.GET.get('mailing_id'))
+    mailing = Mailing.objects.get(id=mailing_id)
+    # customer = mailing.customer
+    mailing_name = mailing.mailing_name
+    logging.error(mailing)
+    # mailing_dropoff_date = mailing.mailing_dropoff_date
+    # job_number = mailing.job_number
+    stats_dict = {}
+    zip3_list = mailing.mailpiece_set.all().order_by('zip3').values_list('zip3',flat=True).distinct()
+    zip3_list = list(zip3_list)
+    zip5_list = mailing.mailpiece_set.all().order_by('zip5').values_list('zip5',flat=True).distinct()
+    zip5_list = list(zip5_list)
+    pieces = mailing.mailpiece_set.all().order_by('zip5')
+    total_pieces = pieces.count()
+    stats_dict_zip3 = {}
+    total = round(pieces.filter(
+        anticipatedDeliveryDate__isnull=False).count() / pieces.count() * 100, 2)
+    for zip_partial in zip3_list:
+        stats_dict[zip_partial] = round(pieces.filter(
+            anticipatedDeliveryDate__isnull=False, zip3__exact=zip_partial).count() / pieces.filter(
+            zip3__exact=zip_partial).count() * 100, 2)
+    # for zip_partial in zip5_list:
+    #     stats_dict[zip_partial] = round(pieces.filter(
+    #         anticipatedDeliveryDate__isnull=False, zip5__exact=zip_partial).count() / pieces.filter(
+    #         zip5__exact=zip_partial).count() * 100)
+    # models.Shop.objects.order_by().values_list('city').distinct()
+    # pieces = mailing.mailpiece_set.all().order_by('zip5').values()
+    # for piece in pieces:
+
+    return render(request, 'mailing_stats.html',
+                  {'zip3s': zip3_list, 'zip5s': zip5_list, 'mailing': mailing, 'stats': stats_dict, 'total': total, 'total_pieces': total_pieces})
