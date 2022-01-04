026a2bc9f6e6fd3175598c6b2da7ed341848f28c
Drake Dwornik <drake@drakedwornik.com>
2022-01-03 21:53:26 -0500

    heroku stuff

--- USPS_Mail_Tracker/settings.py
+++ USPS_Mail_Tracker/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
