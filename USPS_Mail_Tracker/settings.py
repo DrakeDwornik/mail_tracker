6ce43151fb5d830520ec1b892ac9c7076c21c925
Drake Dwornik <drake@drakedwornik.com>
2022-01-04 00:32:16 -0500

    log everything

--- USPS_Mail_Tracker/settings.py
+++ USPS_Mail_Tracker/settings.py
@@ -131,6 +131,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
